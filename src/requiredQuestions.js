e4b59b6794ecc94f214048e5b73d900638f04ba0
Helen <grace.anderson.au@gmail.com>
2022-04-28 00:02:07 +1000

    Completed updating questions to functions.

--- src/requiredQuestions.js
+++ src/requiredQuestions.js
@@ -1,13 +1,13 @@
 //validate user input entered in questions
 function requiredQuestions(errorMessage) {
-    return (userInput) => {
-      if (userInput) {
-        return true;
-      } else {
-        console.log(errorMessage);
-        return false;
-      }
-    };
-  }
-  
-  module.exports = requiredQuestions;
+  return (userInput) => {
+    if (userInput) {
+      return true;
+    } else {
+      console.log(errorMessage);
+      return false;
+    }
+  };
+}
+
+module.exports = requiredQuestions;
