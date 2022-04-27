62edabfec7f338a77ab4359ff8537accf2efe283
Helen <grace.anderson.au@gmail.com>
2022-04-27 18:54:00 +1000

    rough draft of team manager, engineer, intern data entry logic

--- index.js
+++ index.js
@@ -6,111 +6,234 @@ const fs = require("fs");
 
 //require local modules
 const requiredQuestions = require("./src/requiredQuestions");
+const { listenerCount } = require("process");
 
 //array of questions for user input
 const questions = () => {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: `What is the team manager's name? (Required)`,
-        validate: requiredQuestions("Team manager name is required"),
-      },
-      {
-        type: "input",
-        name: "name",
-        message: `What is the team manager's name? (Required)`,
-        validate: requiredQuestions("Team manager name is required"),
-      },
-      {
-        type: "input",
-        name: "github",
-        message: `What is your Github username? (Required)`,
-        validate: requiredQuestions("Your Github username is required"),
-      },
-      {
-        type: "input",
-        name: "email",
-        message: `What is your email? (Valid email required)`,
-        validate: (emailInput) => {
-          const validRegex =
-            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-          if (emailInput) {
-            if (emailInput.match(validRegex)) {
-              return true;
-            } else {
-              console.log(`: ${emailInput} is an invalid email address!`);
-              return false;
-            }
+  return inquirer.prompt([
+    //team manager data input
+    {
+      type: "input",
+      name: "mgrName",
+      message: `What is the team manager's name? (Required)`,
+      validate: requiredQuestions("Team manager's name is required"),
+    },
+    {
+      type: "input",
+      name: "mgrEmployeeId",
+      message: `What is the team manager's employee ID? (Required, numbers only)`,
+      validate: (employeeId) => {
+        const numberRegex = /^\d+$/;
+        if (employeeId) {
+          if (employeeId.match(numberRegex)) {
+            return true;
           } else {
-            console.log("Your email address is required");
+            console.log(
+              `: ${employeeId} is an invalid employee ID. The employee ID should contain numbers only`
+            );
             return false;
           }
-        },
+        } else {
+          console.log("Employee ID is required");
+          return false;
+        }
       },
-      {
-        type: "input",
-        name: "title",
-        message: `What is your project's title? (Required)`,
-        validate: requiredQuestions("Your project title is required"),
+    },
+    {
+      type: "input",
+      name: "mgrEmail",
+      message: `What is the team manager's email address? (Valid email required)`,
+      validate: (emailInput) => {
+        const validRegex =
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (emailInput) {
+          if (emailInput.match(validRegex)) {
+            return true;
+          } else {
+            console.log(`: ${emailInput} is an invalid email address!`);
+            return false;
+          }
+        } else {
+          console.log("Email address is required");
+          return false;
+        }
       },
-      {
-        type: "input",
-        name: "description",
-        message: `Briefly describe your project (Required)`,
-        validate: requiredQuestions("Your project description is required"),
+    },
+    {
+      type: "input",
+      name: "mgrOfficeNo",
+      message: `What is the team manager's office number? (Required, numbers only)`,
+      validate: (mgrOfficeNo) => {
+        const numberRegex = /^\d+$/;
+        if (mgrOfficeNo) {
+          if (mgrOfficeNo.match(numberRegex)) {
+            return true;
+          } else {
+            console.log(
+              `: ${mgrOfficeNo} is an invalid employee ID. The office number should contain numbers only`
+            );
+            return false;
+          }
+        } else {
+          console.log("Team manager's office number is required");
+          return false;
+        }
       },
-      {
-        type: "editor",
-        name: "installation",
-        message: `Write your installation instructions`,
+    },
+    //end team manager input
+    // choice to 1. enter engineer 2. enter intern 3. finish building team
+    {
+      type: "list",
+      name: "nextEntry",
+      message:
+        "Choose to enter an engineer or an intern or to finish building the team",
+      choices: ["Engineer", "Intern", "Finish"],
+    },
+    //1. enter engineer
+    {
+      type: "input",
+      name: "engName",
+      message: `What is the engineer's name? (Required)`,
+      when(answer) {
+        return answer.nextEntry === "Engineer";
       },
-      {
-        type: "editor",
-        name: "usage",
-        message: `Add your usage information`,
+      validate: requiredQuestions("Engineer's name is required"),
+    },
+    {
+      type: "input",
+      name: "engEmployeeId",
+      message: `What is the engineer's employee ID? (Required, numbers only)`,
+      when(answer) {
+        return answer.nextEntry === "Engineer";
       },
-      {
-        type: "list",
-        message: "Select a licence for your project",
-        name: "license",
-        choices: [
-          `None`,
-          `Apache License 2.0`,
-          `BSD 2-Clause`,
-          `BSD 3-Clause`,
-          `Boost Software License 1.0`,
-          `Creative Commons Zero v1.0 Universal`,
-          `Eclipse Public License 2.0`,
-          `GNU Affero General Public License v3.0`,
-          `GNU General Public License v2.0`,
-          `GNU General Public License v3.0`,
-          `GNU Lesser General Public License v2.1`,
-          `MIT`,
-          `Mozilla Public License 2.0`,
-          `The Unlicense`,
-        ],
+      validate: (employeeId) => {
+        const numberRegex = /^\d+$/;
+        if (employeeId) {
+          if (employeeId.match(numberRegex)) {
+            return true;
+          } else {
+            console.log(
+              `: ${employeeId} is an invalid employee ID. The employee ID should contain numbers only`
+            );
+            return false;
+          }
+        } else {
+          console.log("Employee ID is required");
+          return false;
+        }
       },
-      {
-        type: "editor",
-        name: "contribution",
-        message: `Describe your contribution guidelines`,
+    },
+    {
+      type: "input",
+      name: "engEmail",
+      message: `What is the engineers's email address? (Valid email required)`,
+      when(answer) {
+        return answer.nextEntry === "Engineer";
       },
-      {
-        type: "editor",
-        name: "test",
-        message: `Add your test instructions`,
+      validate: (emailInput) => {
+        const validRegex =
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (emailInput) {
+          if (emailInput.match(validRegex)) {
+            return true;
+          } else {
+            console.log(`: ${emailInput} is an invalid email address!`);
+            return false;
+          }
+        } else {
+          console.log("Email address is required");
+          return false;
+        }
       },
-    ]);
-  };
-  
-  // initialize app, then use user input to write readme file
-  const init = () => {
-    questions()
-      .then((answers) => fs.writeFileSync(`README.md`, generateMarkdown(answers)))
-      .then(() => console.log("Successfully created README.md"))
-      .catch((err) => console.error(err));
-  };
-  
-  // kick off (initialize) app
-  init();
+    },
+    {
+      type: "input",
+      name: "github",
+      message: `What is the engineer's Github username? (Required)`,
+      when(answer) {
+        return answer.nextEntry === "Engineer";
+      },
+      validate: requiredQuestions("Github username is required"),
+    },
+    /////end engineer
+    
+    //2. enter intern
+    {
+      type: "input",
+      name: "internName",
+      message: `What is the intern's name? (Required)`,
+      when(answer) {
+        return answer.nextEntry === "Intern";
+      },
+      validate: requiredQuestions("Intern's name is required"),
+    },
+    {
+      type: "input",
+      name: "internEmployeeId",
+      message: `What is the intern's employee ID? (Required, numbers only)`,
+      when(answer) {
+        return answer.nextEntry === "Intern";
+      },
+      validate: (employeeId) => {
+        const numberRegex = /^\d+$/;
+        if (employeeId) {
+          if (employeeId.match(numberRegex)) {
+            return true;
+          } else {
+            console.log(
+              `: ${employeeId} is an invalid employee ID. The employee ID should contain numbers only`
+            );
+            return false;
+          }
+        } else {
+          console.log("Employee ID is required");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: `What is the intern's email address? (Valid email required)`,
+      when(answer) {
+        return answer.nextEntry === "Intern";
+      },
+      validate: (emailInput) => {
+        const validRegex =
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (emailInput) {
+          if (emailInput.match(validRegex)) {
+            return true;
+          } else {
+            console.log(`: ${emailInput} is an invalid email address!`);
+            return false;
+          }
+        } else {
+          console.log("Email address is required");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "internSchool",
+      message: `What is the intern's school (Required)`,
+      when(answer) {
+        return answer.nextEntry === "Intern";
+      },
+      validate: requiredQuestions("Intern's school is required"),
+    },
+    /////end intern
+  ]);
+};
+
+// initialize app, then use user input to write readme file
+const init = () => {
+  questions()
+    .then((answers) => fs.writeFileSync(`README.md`, generateMarkdown(answers)))
+    .then(() => console.log("Successfully created README.md"))
+    .catch((err) => console.error(err));
+};
+
+// kick off (initialize) app
+init();
