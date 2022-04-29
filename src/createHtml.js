790b785ec903758cac0761d53510f9e5e2209532
Helen <grace.anderson.au@gmail.com>
2022-04-29 22:51:16 +1000

    Added practice test, tidy up html

--- src/createHtml.js
+++ src/createHtml.js
@@ -28,19 +28,22 @@ const createEmployeeCards = (teamArray) => {
 const createTeamManager = (teamManagerRole) => {
   return teamManagerRole
     .map((teamManager) => {
-      return `<div class="col d-flex justify-content-center pe-0 me-4 ps-0 ms-4">
-      <div class="card shadow-lg mb-5" style="width: 18rem;">
-            <div class="card-body text-white bg-success">
-                <h2 class="card-title lh-base">${teamManager.returnName()}</h2>
-                <h4 class="card-title"><i class="bi bi-briefcase-fill"></i>  Team Manager</h4>
+      return `
+      <div class="col d-flex justify-content-center pe-0 me-4 ps-0 ms-4">
+        <div class="card shadow-lg mb-5" style="width: 18rem;">
+          <div class="card-body pt-0 ps-0 pe-0">
+            <div class="text-white bg-success px-4 pt-1 pb-2">
+              <h2 class="card-title lh-base">${teamManager.returnName()}</h2>
+              <h4 class="card-title"><i class="bi bi-briefcase-fill"></i>  Team Manager</h4>
             </div>
             <ul class="list-group list-group-flush px-2">
-                <li class="list-group-item">Id: ${teamManager.returnEmployeeId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:">${teamManager.returnEmail()}</a></li>
-                <li class="list-group-item">Office number: ${teamManager.returnOfficeNo()}</li>
+              <li class="list-group-item">Id: ${teamManager.returnEmployeeId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:">${teamManager.returnEmail()}</a></li>
+              <li class="list-group-item">Office number: ${teamManager.returnOfficeNo()}</li>
             </ul>
+          </div>
         </div>
-    </div>`;
+      </div>`;
     })
     .join("");
 };
@@ -48,19 +51,22 @@ const createTeamManager = (teamManagerRole) => {
 const createEngineer = (engineerRole) => {
   return engineerRole
     .map((engineer) => {
-      return `<div class="col d-flex justify-content-center pe-0 me-4 ps-0 ms-4">
-      <div class="card shadow-lg mb-5" style="width: 18rem;">
-        <div class="card-body text-white bg-info">
-            <h2 class="card-title lh-base">${engineer.returnName()}</h2>
-            <h4 class="card-title"><i class="bi bi-gear-fill"></i>  Engineer</h4>
+      return `
+      <div class="col d-flex justify-content-center pe-0 me-4 ps-0 ms-4">
+        <div class="card shadow-lg mb-5" style="width: 18rem;">
+          <div class="card-body pt-0 ps-0 pe-0">
+            <div class="text-white bg-info px-4 pt-1 pb-2">
+              <h2 class="card-title lh-base">${engineer.returnName()}</h2>
+              <h4 class="card-title"><i class="bi bi-gear-fill"></i>  Engineer</h4>
+            </div>
+            <ul class="list-group list-group-flush px-2">
+              <li class="list-group-item">Id: ${engineer.returnEmployeeId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:">${engineer.returnEmail()}</a></li>
+              <li class="list-group-item">GitHub: <a target="_blank" href="https://github.com/${engineer.returnGitHub()}">${engineer.returnGitHub()}</a></li>
+            </ul>
+          </div>
         </div>
-        <ul class="list-group list-group-flush px-2">
-            <li class="list-group-item">Id: ${engineer.returnEmployeeId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:">${engineer.returnEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a target="_blank" href="https://github.com/${engineer.returnGitHub()}">${engineer.returnGitHub()}</a></li>
-        </ul>
-    </div>
-</div>`;
+      </div>`;
     })
     .join("");
 };
@@ -68,29 +74,33 @@ const createEngineer = (engineerRole) => {
 const createIntern = (internRole) => {
   return internRole
     .map((intern) => {
-      return `<div class="col d-flex justify-content-center pe-0 me-4 ps-0 ms-4">
-      <div class="card shadow-lg mb-5" style="width: 18rem;">
-        <div class="card-body text-white bg-danger">
-            <h2 class="card-title lh-base">${intern.returnName()}</h2>
-            <h4 class="card-title"><i class="bi bi-book-fill"></i>  Intern</h4>
+      return `
+      <div class="col d-flex justify-content-center pe-0 me-4 ps-0 ms-4">
+        <div class="card shadow-lg mb-5" style="width: 18rem;">
+          <div class="card-body pt-0 ps-0 pe-0">
+            <div class="text-white bg-danger px-4 pt-1 pb-2">
+              <h2 class="card-title lh-base">${intern.returnName()}</h2>
+              <h4 class="card-title"><i class="bi bi-book-fill"></i>  Intern</h4>
+            </div>
+            <ul class="list-group list-group-flush px-2">
+              <li class="list-group-item">Id: ${intern.returnEmployeeId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:">${intern.returnEmail()}</a></li>
+              <li class="list-group-item">School: ${intern.returnSchool()}</li>
+            </ul>
+          </div>
         </div>
-        <ul class="list-group list-group-flush px-2">
-            <li class="list-group-item">Id: ${intern.returnEmployeeId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:">${intern.returnEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.returnSchool()}</li>
-        </ul>
-    </div>
-    </div>`;
+      </div>`;
     })
     .join("");
 };
 
-//export card for use by createEmployeeCards to create html page
+//export card for use by createEmployeeCards to create
 module.exports = (cards) => {
   return ` 
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -100,21 +110,22 @@ module.exports = (cards) => {
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
-</head>
+  </head>
 
-<body>
+  <body>
+
     <header class="container-fluid bg-primary py-1">
         <h1 class="display-3 text-center fw-bolder lh-lg text-white">TEAM ROSTER</h1>
     </header>
     <main class="container-fluid d-flex justify-content-center my-5">
-        <div class="row ms-0 me-0">
-            ${createEmployeeCards(cards)}
-        </div>
+      <div class="row ms-0 me-0">
+          ${createEmployeeCards(cards)}
+      </div>
     </main> 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-</body>
+  </body>
 </html>
     `;
 };
